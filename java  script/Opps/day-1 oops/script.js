f02a47baed04911fe42e42d839e57a0476ee2861
Surya <mrsurajyadavdj@gmail.com>
2025-06-11 17:54:47 +0530

    Add files via upload

---
+++ java  script/Opps/day-1 oops/script.js
@@ -0,0 +1,106 @@
+// create a class for Student with minimum 5 attributes, create 3
+// defferent objects with meaning ful name, and set and get all
+// information.
+
+// class Student {
+//     name;
+//     age;
+//     grade;
+//     hobbie;
+//     mnum;
+
+
+//     info(name, age, grade, hobbie, mnum) {
+//         this.name = name;
+//         this.age = age;
+//         this.grade = grade;
+//         this.hobbie = hobbie;
+//         this.mnum = mnum;
+//     }
+// }
+
+// const suraj =new Student();
+// suraj.info("suraj", "21", "A", "playing", "7069");
+// const yatin =new Student();
+// yatin.info("yatin", "22", "b", "lifting", "6352");
+// const tokyo=new Student();
+// tokyo.info("tokyo", "20", "c", "book reading", "9839");
+
+// document.writeln(tokyo.grade);
+
+
+
+// ------------------Q2--------------------------
+// create a class for Car with minimum 5 attributes, create 3
+// defferent objects with meaning ful name, and set and get all
+// information.
+
+
+// class Car{
+//     carname;
+//     carlogo;
+//     carcompany;
+//     energytype;
+//     carsitter;
+
+//     info(carname,carlogo,carcompany,energytype,carsitter){
+//         this.carname=carname;
+//         this.carlogo=carlogo;
+//         this.carcompany=carcompany;
+//         this.energytype=energytype;
+//         this.carsitter=carsitter;
+//     }
+// }
+
+// const eartiga = new Car();
+// eartiga.info("Ertiga-570","maruti-suzuki","maruti","petrol+desile","7sitter")
+// // document.writeln(eartiga.carname);
+// const BMWiX=new Car();
+// BMWiX.info("BMWiX","BMW","BMW","Electric","only4");
+// // document.writeln(BMWiX.carname);
+
+// const tatanexon = new Car();
+// tatanexon.info("tatanexon","tata","TT","petrol","4-sitter")
+// document.writeln(tatanexon.carsitter);
+
+
+
+
+
+
+// create class for ATM, with checkBalance, withdraw, deposit.
+
+
+class ATM{
+    //usefull atmcard,pin,balence
+    ATMcard;
+    pin;
+    balence;
+    withdrow;
+    deposit;
+
+    checkbalence(ATMcard,pin,balence,withdrow,deposit){
+        this.ATMcard=ATMcard;
+        document.writeln("your atmcard : " +ATMcard+"<br>")
+
+        this.pin=pin;
+        document.writeln("your pinnumber : " +pin+"<br>")
+
+        this.deposit=deposit;
+        document.writeln("you deposit : " + deposit+"<br>")
+
+        this.balence=(balence + deposit);
+        document.writeln("your letest balnece : " +balence+"<br>")
+
+        this.withdrow=withdrow;
+        
+        document.writeln("your withdrowl balece :"+ withdrow +"<br>")
+        
+    }
+}
+
+const card = new ATM();
+card.checkbalence("sbi","1547",20000,5000,1000);
+
+
+
