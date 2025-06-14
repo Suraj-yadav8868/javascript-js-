9872791bdbe48970806ba377853e569d0346d933
Surya <mrsurajyadavdj@gmail.com>
2025-06-14 17:49:01 +0530

    Add files via upload

--- java  script/DOM/Text Analyzer/script.js
+++ java  script/DOM/Text Analyzer/script.js
@@ -1,7 +1,8 @@
 const addtext =document.getElementById("addtext");
 const addtextbtn = document.getElementById("add-text-btn");
 const preview = document.getElementById("preview");
-const toupper = document.getElementById("to-upper")
+const toupper = document.getElementById("to-upper");
+const totalcount =document.getElementById("totalcount");
 
 addtextbtn.addEventListener('click',()=>{
     preview.textContent=addtext.value
@@ -12,22 +13,21 @@ toupper.addEventListener('click',()=>{
     preview.textContent =newtext.toUpperCase();
 })
 
+totalcount.addEventListener('click', () => {
+  const newText = addtext.value;
+  preview.textContent = newText;
+  char.textContent = "Total Characters : " + newText.length;
 
+  const textArray = newText.split(" ");
+  words.textContent = "Total Words : " + textArray.length;
+  time.textContent = "Time to read : " + textArray.length * 0.25 + "s";
+});
 
+const textArray = newText.trim().split(/\s+/); // split by any whitespace
+const wordCount = textArray.filter(word => word !== "").length;
 
+let seconds = wordCount * 0.25;
+let mins = Math.floor(seconds / 60);
+let sec = Math.floor(seconds % 60);
+time.textContent = `Reading Time: ${mins}m ${sec}s`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(preview)
